a6e4c95c280a64dcb2909767bed7e4fb332f634a
sbur4 <serhii.burch@gmail.com>
2021-08-02 00:30:52 +0300

    update

--- src/main/java/Test3.java
+++ src/main/java/Test3.java
@@ -3,20 +3,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Req1 {
+import java.util.concurrent.TimeUnit;
 
-    @Test(description = "Verify user is successfully logged in with appropriate credentials")
+import org.testng.asserts.SoftAssert;
+
+public class Req3 {
+
+    @Test
 
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
-        //System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\chromdriver_93.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -24,33 +24,47 @@ public class Req1 {
 
         try {
 
-            // 1. Open the browser and access the ‘https://training.by’ URL.
             driver.get("https://training.by");
 
+            //positive
+            Assert.assertTrue(Boolean.parseBoolean(driver.getCurrentUrl()));
+
+
             WebElement clickSignIn = driver.findElement(By.className("header-auth__signin"));
-            // 2. Click on ‘Sign In’ button in upper right corner of the window.
             clickSignIn.click();
-            // 3. Enter valid user’s mail
+
+            //Hard Assertion negative
+            Assert.assertFalse(driver.findElement(By.className("header-auth__signin")).isSelected());
+
             WebElement enterUserGoogle = driver.findElement(By.id("zocial-google"));
             enterUserGoogle.click();
             WebElement enterUserMail = driver.findElement(By.id("identifierId"));
             enterUserMail.sendKeys("serhii.burch@gmail.com");
-            // 4. Click Continue button
+
+            //Boundary values positive
+            int text = enterUserMail.getText().length();
+            boolean maxText = text <= 64;
+            Assert.assertEquals(maxText, true);
+
             WebElement enterMailNext = driver.findElement(By.tagName("button"));
             enterMailNext.click();
-            // 5. Enter valid password
-            // ?
+
+            //positive
+            Assert.assertTrue(driver.findElement(By.tagName("button")).equals("button"));
+
             WebElement enterUserPass = driver.findElement(By.name("password"));
             enterUserPass.sendKeys("dHeTTp9\\HuH");
-            // 6. Click ‘Sign In’ button
             WebElement enterPassNext = driver.findElement(By.tagName("button"));
             enterPassNext.click();
 
             WebElement userName = driver.findElement(By.className("user-info__name"));
-            Assert.assertTrue(userName.isDisplayed(), "Username is NOT displayed");
+
+            //Soft Assertion negative
+            SoftAssert softAssert = new SoftAssert();
+            softAssert.assertEquals( driver.findElement(By.name("password")), "user", "First soft assert failed");
+            softAssert.assertAll();
 
         } finally {
-
             driver.close();
             driver.quit();
         }
